5999862c460f7be98c6149cc0edf95b3a68a610f
Kyanh Pham <kyanh.pham@mths.ca>
2022-10-18 14:26:59 +0000

    Guess the number in CPP

--- guessNumber.cpp
+++ guessNumber.cpp
@@ -15,21 +15,18 @@ int main() {
     std::mt19937 rgen(rseed());
     std::uniform_int_distribution<int> idist(0, 9);
     randomNumber = idist(rgen);
-    std::cout << randomNumber << std::endl;
-    std::cout << "";
 
     // Input
     std::cout << "Enter the number between 0-9: ";
     std::cin >> guessedNumber;
+    std::cout << std::endl;
 
     // Process and Output
-    randomNumber = idist(rgen);
     if (guessedNumber == randomNumber) {
         std::cout << "You guessed right";
     } else {
-        std::cout << "You guessed wrong, the number was " << "" 
-        << randomNumber << "" << std::endl;
+        std::cout << "You guessed wrong, the number was " << ""
+         << randomNumber << "" << std::endl;
     }
-
     std::cout << "\nDone.";
 }
